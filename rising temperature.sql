06ad915c95270078ce92edb0bd4a4075908b4972
Shiv Prakash Anand <shivakp2111@gmail.com>
2017-09-01 14:28:51 +0530

    Update rising temperature.sql

--- rising temperature.sql
+++ rising temperature.sql
@@ -1,5 +1,19 @@
 # Write your MySQL query statement below
 
+-- SCHEMA
+/**
+
+Create table If Not Exists Weather (Id int, Date date, Temperature int);
+Truncate table Weather;
+insert into Weather (Id, Date, Temperature) values ('1', '2015-01-01', '10');
+insert into Weather (Id, Date, Temperature) values ('2', '2015-01-02', '25');
+insert into Weather (Id, Date, Temperature) values ('3', '2015-01-03', '20');
+insert into Weather (Id, Date, Temperature) values ('4', '2015-01-04', '30');
+
+**/
+
+-- query
+
 SELECT W1.ID 
 FROM WEATHER W1, WEATHER W2
 WHERE W1.TEMPERATURE > W2.TEMPERATURE AND

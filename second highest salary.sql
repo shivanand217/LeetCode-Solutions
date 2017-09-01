05de8086630a0c353b17d67e554c8338db7803d9
Shiv Prakash Anand <shivakp2111@gmail.com>
2017-09-01 14:26:18 +0530

    Update second highest salary.sql

--- second highest salary.sql
+++ second highest salary.sql
@@ -2,5 +2,14 @@
 
 # SELECT MAX(SALARY) FROM EMPLOYEE AS 'SecondHighestSalary' WHERE SALARY < (SELECT MAX(SALARY) FROM EMPLOYEE)
 
+/** SCHEMA
+
+  Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+**/
+
 SELECT MAX(SALARY) as 'SecondHighestSalary' FROM EMPLOYEE WHERE SALARY < (SELECT MAX(SALARY) FROM EMPLOYEE);
 

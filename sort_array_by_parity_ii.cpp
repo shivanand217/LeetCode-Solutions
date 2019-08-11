7f269eb3176ce8b4dcde1716a102555b758edcf8
Shiv Prakash <shivakp2111@gmail.com>
2019-08-11 18:56:57 +0530

    without extra space

--- sort_array_by_parity_ii.cpp
+++ sort_array_by_parity_ii.cpp
@@ -1,3 +1,4 @@
+// With extra space
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& A) {
@@ -17,6 +18,28 @@ public:
         }
         for(int i=0;i<index1.size();i++){
             swap(A[index1[i]],A[index2[i]]);
+        }
+        
+        return A;
+    }
+};
+
+// Without extra space
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        
+        int even_index=0,odd_index=1;
+        
+        while(even_index < A.size()){
+            
+            if(A[even_index]%2 == 1){
+                while(A[odd_index]%2 == 1){
+                    odd_index+=2;
+                }
+                swap(A[even_index],A[odd_index]);
+            }
+            even_index+=2;
         }
         
         return A;

947f9ae18892fde44bdf9da5fcb16aafd529ba6d
Shiv Prakash <shiv@kruzr.co>
2019-11-11 15:19:56 +0530

    linked list cycle problem

---
+++ LinkedListCycle2.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL){
+            return NULL;
+        }
+        ListNode *slow = head, *fast = head;
+        ListNode *cycleNode = NULL;
+        
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast){
+                break;
+            }
+        }
+        if(fast == NULL || fast->next == NULL){
+            return NULL;
+        }
+        ListNode *temp1 = slow, *temp2 = head;
+        
+        while(temp1 != temp2){
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        return temp1;
+    }
+};
